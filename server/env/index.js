f09bc08a3057586e2fa598475100d08510379aae
superburrito <chuayaohui@gmail.com>
2017-04-20 16:09:00 +0800

    staging

--- server/env/index.js
+++ server/env/index.js
@@ -2,8 +2,10 @@ const path = require('path');
 const devConfigPath = path.join(__dirname, './dev.js');
 const prodConfigPath = path.join(__dirname, './prod.js');
 
-if (process.env.NODE_ENV === 'production') {
-	module.exports = require(prodConfigPath);
-} else if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
 	module.exports = require(devConfigPath);
+} else if (process.env.NODE_ENV === 'staging') {
+	module.exports = require(devConfigPath)
+} else if (process.env.NODE_ENV === 'production') {
+	module.exports = require(prodConfigPath);
 }
--- server/server.js
+++ server/server.js
@@ -22,43 +22,43 @@ app.listen(port, (err) => {
 	} else {
 		return db.sync({ force: true })
 		.then(() => {
-			const User = db.model('user');
-			const Membership = db.model('membership');
-			const u1Prom = User.create({
-				id: 1,
-				fbId: 1021108506,
-				name: "Joshua Wong",
-				linkedIn: "https://sg.linkedin.com/in/joshua-wong-566397b6",
-				email: "jwong@hotmail.com",
-				class: "2017",
-				src: "https://scontent.xx.fbcdn.net/v/t31.0-1/c173.311.1229.1229/s960x960/13475034_10208628672127173_5988747467162591874_o.jpg?oh=affc50395ab19334fcf32ad56c38ad68&oe=59938EE9",
-				lat: 1.30,
-				lng: 103.82,
-				description: "TGS was a mistake",
-				role: "Teaching Associate",
-				employer: "Learning Lab",
-				city: "Singapore",
-				major: "PPE"
-			})
-			const m1Prom = Membership.create({
-				club: "Community of Learning"
-			})			
-			const m2Prom = Membership.create({
-				club: "Improv Club"
-			})
-			return Promise.all([u1Prom, m1Prom, m2Prom])
-			.spread((user1, membership1, membership2) => {
-				return user1.addMembership(membership1)
-				.then(user1.addMembership(membership2))
-			})
-			.then(() => {
-				console.log("Database reset and models synced.");		
-			})
+			if (process.env.NODE_ENV === 'development') {
+				const User = db.model('user');
+				const Membership = db.model('membership');
+				const u1Prom = User.create({
+					id: 1,
+					fbId: 1021108506,
+					name: "Joshua Wong",
+					linkedIn: "https://sg.linkedin.com/in/joshua-wong-566397b6",
+					email: "jwong@hotmail.com",
+					class: "2017",
+					src: "https://scontent.xx.fbcdn.net/v/t31.0-1/c173.311.1229.1229/s960x960/13475034_10208628672127173_5988747467162591874_o.jpg?oh=affc50395ab19334fcf32ad56c38ad68&oe=59938EE9",
+					lat: 1.30,
+					lng: 103.82,
+					description: "TGS was a mistake",
+					role: "Teaching Associate",
+					employer: "Learning Lab",
+					city: "Singapore",
+					major: "PPE"
+				})
+				const m1Prom = Membership.create({
+					club: "Community of Learning"
+				})			
+				const m2Prom = Membership.create({
+					club: "Improv Club"
+				})
+				return Promise.all([u1Prom, m1Prom, m2Prom])
+				.spread((user1, membership1, membership2) => {
+					return user1.addMembership(membership1)
+					.then(user1.addMembership(membership2))
+				})
+			}
 		}, (err) => {
 			console.log(err);
-			console.log("Database sync failed.");
+			console.log("Database setup failed.");
 		})
 		.then(() => {
+			console.log("Database reset and models synced.");		
 			console.log("YNAN server up and listening at: " + port);
 		})
 	}
