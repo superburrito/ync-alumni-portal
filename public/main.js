7eb4e0a4eba08a48769fa2975481f7b7dfa5ee16
superburrito <chuayaohui@gmail.com>
2017-04-21 00:17:06 +0800

    halcyon

--- public/main.js
+++ public/main.js
@@ -520,11 +520,72 @@ app.factory('MapStyleFac', function () {
 'use strict';
 
 app.controller('ProfileCtrl', function ($mdDialog, $scope, $rootScope, GeneralFac) {
+
+	function loadMyProfile() {
+		$scope.myProfile = JSON.parse(localStorage.getItem('ynan-profile'));
+	}
+	loadMyProfile();
+
 	$scope.cancel = function () {
 		$mdDialog.cancel();
 	};
 	$scope.currProfile = $rootScope.currProfile;
 	$scope.triggerFBDialog = GeneralFac.triggerFBDialog;
+});
+"use strict";
+
+app.controller("UpdateCtrl", function ($scope, $state, GeneralFac) {
+	$scope.bg = '/media/yalenus_options.jpg';
+
+	$scope.submit = function () {
+		convertClubs();
+		if ($scope.inputCoordAgree) {
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(addProfile, navFailureDialog);
+			} else {
+				navFailureDialog();
+				addProfile(null);
+			}
+		} else {
+			addProfile(null);
+		}
+	};
+
+	function convertClubs() {
+		if ($scope.inputUser.clubsStr && $scope.inputUser.clubsStr.length > 0) {
+			var clubStrs = $scope.inputUser.clubsStr.split(",");
+			$scope.inputUser.clubStrs = clubStrs.map(function (clubStr) {
+				if (clubStr[0] == " ") {
+					return clubStr.slice(1);
+				} else if (clubStr[clubStr.length - 1] == " ") {
+					return clubStr.slice(0, -1);
+				} else {
+					return clubStr;
+				}
+			});
+		} else {
+			$scope.inputUser.clubStrs = [];
+		}
+	}
+
+	function addProfile(posObj) {
+		if (posObj && posObj.coords) {
+			$scope.inputUser.lat = posObj.coords.latitude;
+			$scope.inputUser.lng = posObj.coords.longitude;
+			$scope.nextState = "map";
+		} else {
+			$scope.nextState = "grid";
+		}
+		GeneralFac.updateUser($scope.inputUser).then(function () {
+			return GeneralFac.updateUserClubs($scope.inputUser.clubStrs);
+		}).then(function () {
+			$state.go($scope.nextState);
+		}, GeneralFac.errorDialog);
+	}
+
+	function navFailureDialog() {
+		$mdDialog.show($mdDialog.alert().parent(angular.element(document.body)).clickOutsideToClose(true).title('Oh no.').textContent("We were unable to retrieve your geolocation.").ariaLabel('Failed Nav Dialog').ok("OK"));
+	}
 });
 "use strict";
 
@@ -630,59 +691,4 @@ app.factory('APIInterceptor', function () {
  	}*/
 
 	return APIInterceptor;
-});
-"use strict";
-
-app.controller("UpdateCtrl", function ($scope, $state, GeneralFac) {
-	$scope.bg = '/media/yalenus_options.jpg';
-
-	$scope.submit = function () {
-		convertClubs();
-		if ($scope.inputCoordAgree) {
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(addProfile, navFailureDialog);
-			} else {
-				navFailureDialog();
-				addProfile(null);
-			}
-		} else {
-			addProfile(null);
-		}
-	};
-
-	function convertClubs() {
-		if ($scope.inputUser.clubsStr && $scope.inputUser.clubsStr.length > 0) {
-			var clubStrs = $scope.inputUser.clubsStr.split(",");
-			$scope.inputUser.clubStrs = clubStrs.map(function (clubStr) {
-				if (clubStr[0] == " ") {
-					return clubStr.slice(1);
-				} else if (clubStr[clubStr.length - 1] == " ") {
-					return clubStr.slice(0, -1);
-				} else {
-					return clubStr;
-				}
-			});
-		} else {
-			$scope.inputUser.clubStrs = [];
-		}
-	}
-
-	function addProfile(posObj) {
-		if (posObj && posObj.coords) {
-			$scope.inputUser.lat = posObj.coords.latitude;
-			$scope.inputUser.lng = posObj.coords.longitude;
-			$scope.nextState = "map";
-		} else {
-			$scope.nextState = "grid";
-		}
-		GeneralFac.updateUser($scope.inputUser).then(function () {
-			return GeneralFac.updateUserClubs($scope.inputUser.clubStrs);
-		}).then(function () {
-			$state.go($scope.nextState);
-		}, GeneralFac.errorDialog);
-	}
-
-	function navFailureDialog() {
-		$mdDialog.show($mdDialog.alert().parent(angular.element(document.body)).clickOutsideToClose(true).title('Oh no.').textContent("We were unable to retrieve your geolocation.").ariaLabel('Failed Nav Dialog').ok("OK"));
-	}
 });
